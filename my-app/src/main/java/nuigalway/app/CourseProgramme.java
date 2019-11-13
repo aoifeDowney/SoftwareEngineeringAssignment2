ad4f82ced6b7daf4530fcd7ba4193b891a0c64f1
aoifeDowney <aoifestard@gmail.com>
2019-11-13 16:25:58 +0000

    adding course programme class

---
+++ my-app/src/main/java/nuigalway/app/CourseProgramme.java
@@ -0,0 +1,51 @@
+package nuigalway.app;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+public class CourseProgramme {
+	
+	private String name;
+	private ArrayList<Module> modules = new ArrayList<Module>();
+	private ArrayList<Student> students = new ArrayList<Student>();
+	private Date startDate;
+	private Date endDate;
+	
+	//Constructor 
+	public CourseProgramme() {
+		
+	}
+	
+	//GETTER AND SETTERS
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public ArrayList<Module> getModules() {
+		return modules;
+	}
+	public void setModules(ArrayList<Module> modules) {
+		this.modules = modules;
+	}
+	public ArrayList<Student> getStudents() {
+		return students;
+	}
+	public void setStudents(ArrayList<Student> students) {
+		this.students = students;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+}
